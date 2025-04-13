294238bde9a8c335f76a9b4319d4b4e9d48cbb27
Anastasia Tikhonova <honu347@gmail.com>
2025-04-13 18:12:16 +0200

    add condition for different nav bar color

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import Button from "../ui/Button";
 import NavLink from "./NavLinks";
 
 const NavBar = () => {
+  const pathname = usePathname();
+  const transparentNavbarPaths = ["/", "/donate", "/about/team"];
+  const shouldBeTransparent = transparentNavbarPaths.includes(pathname || "");
+
   const links = [
     {
       label: "Our Impact",
@@ -29,7 +36,16 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="relative w-full bg-[#1C1B23] py-6 px-10">
+    <nav
+      className={`
+        relative w-full py-6 px-10
+        ${
+          shouldBeTransparent
+            ? "absolute top-0 left-0 z-50 bg-[#08001E] backdrop-blur-sm"
+            : "bg-[#181818]"
+        }
+      `}
+    >
       <div className="mx-auto flex items-center justify-between">
         {/* Logo and Score */}
         <Link href="/" className="flex items-center">
